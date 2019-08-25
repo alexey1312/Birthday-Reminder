e24ddfa9f2e57e024f2f1be5b2119e7d8298b4d3
akakoulin <alexey1312@me.com>
2019-08-25 19:02:16 +0700

    merge alarm and master

--- Birthday Reminder/AppDelegate.swift
+++ Birthday Reminder/AppDelegate.swift
@@ -15,12 +15,6 @@ import Contacts
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    
-    
-    
-    
-    
-    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
