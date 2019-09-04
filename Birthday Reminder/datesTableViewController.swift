f07f16527dfd1f567d62187737d7f340b97585b0
akakoulin <alexey1312@me.com>
2019-09-04 21:40:17 +0700

    kak-to tak=(

--- Birthday Reminder/datesTableViewController.swift
+++ Birthday Reminder/datesTableViewController.swift
@@ -12,67 +12,41 @@ import UIKit
 
  
 class datesTableViewController: UITableViewController {
-  
-    private var usersBirthday: Results<Birthday>!
-    
-    let items = try! Realm().objects(Birthday.self).sorted(by: ["userBirthDate", "userFirstName"])
-    var section: [String] {
-        return Set(items.value(forKeyPath: "userBirthDate") as! [String]).sorted()
-    }
 
+    let items = try! Realm().objects(Birthday.self).sorted(by: ["userFirstName"])
+    var sectionNames: [String] {
+        return Set(items.value(forKeyPath: "userFirstName") as! [String]).sorted()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "dateCell")
-        
-        usersBirthday = realm.objects(Birthday.self)
         tableView.tableFooterView = UIView()//Где нет коннтента убирает разделителей полей
     }
-    
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
 
-        return self.usersBirthday.count
+        return self.sectionNames.count
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        let section = usersBirthday[section]
-        let date = section.userBirthDate
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MMMM"
-        return dateFormatter.string(from: date ?? Date())
+
+        return sectionNames[section]
     }
     
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return usersBirthday.count / usersBirthday.count
+        return items.filter("userFirstName == %@", sectionNames[section]).count
         
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell", for: indexPath)
         
-        let headline = usersBirthday[indexPath.section]
-
-        //Конвертация даты
-        let userBirthdayDate = headline.userBirthDate
-        let dateForamaterDate = DateFormatter()
-        let dateFormatreWeekDay = DateFormatter()
-        
-        dateForamaterDate.dateFormat = "yyyy-MM-dd"
-        dateFormatreWeekDay.dateFormat = "EEEE"
-               
-        let dateValueDate = dateForamaterDate.string(from: userBirthdayDate!)
-        let dateValueWeekDay = dateFormatreWeekDay.string(from: userBirthdayDate!)
-        let capitalizedDate = dateValueDate.capitalized
-        let capitalizedWeekday = dateValueWeekDay.capitalized
-        let fullDate = "\(capitalizedWeekday) \(capitalizedDate)"
-        
-        cell.textLabel?.text = headline.userfullName
-        cell.detailTextLabel?.text = fullDate
+        cell.textLabel?.text = items.filter("userFirstName == %@", sectionNames[indexPath.section])[indexPath.row].userBirthDateToString
 
         return cell
     }
